64aac5ed1b3e313def86e4dbfee37812c1ab23e5
Farjey <cvet57@rambler.ru>
2023-08-18 02:52:27 +0300

    Доделал фильтры

--- build/js/editor.js
+++ build/js/editor.js
@@ -1,3 +1,5 @@
+import {setDefaultOptions, effectRadioGroup, setFilter} from './effects.js'
+
 const pageBody = document.querySelector('body')
 const uploadInput = document.querySelector('#upload-file')
 
@@ -5,12 +7,25 @@ const uploadOverlay = document.querySelector('.img-upload__overlay')
 const uploadPreview = uploadOverlay.querySelector('.img-upload__preview > img')
 const uploadModalClose = uploadOverlay.querySelector('#upload-cancel')
 
+const buttonsScale = document.querySelectorAll('.scale__control')
+const scaleValue = document.querySelector('.scale__control--value')
+
+const Scale = {
+  MAX: 100,
+  MIN: 25,
+  STEP: 25,
+}
+
 uploadInput.addEventListener ('change', () => {
   uploadPreview.src = URL.createObjectURL(uploadInput.files[0])
   uploadOverlay.classList.remove('hidden')
   pageBody.classList.add('modal-open')
   document.addEventListener('keydown', onModalKeydown)
   uploadModalClose.addEventListener('click', onModalCloseClick)
+  setDefaultOptions()
+  for (let i = 0; i < effectRadioGroup.length; i++){
+    effectRadioGroup[i].addEventListener('change', setFilter);
+  }
 })
 
 const closeModal = () => {
@@ -20,6 +35,9 @@ const closeModal = () => {
   uploadInput.value = '';
   document.removeEventListener('keydown', onModalKeydown)
   uploadModalClose.removeEventListener('click', onModalCloseClick)
+  for (let i = 0; i < effectRadioGroup.length; i++) {
+    effectRadioGroup[i].removeEventListener('change', setFilter);
+  }
 }
 
 const onModalCloseClick = () => {
@@ -30,3 +48,16 @@ const onModalKeydown = (evt) => {
   evt.preventDefault();
   if (evt.code === ('Escape' || 'Esc')) closeModal();
 }
+
+[...buttonsScale].map( (btn) => {
+  btn.addEventListener('click', (evt) => {
+    let scale = parseInt(scaleValue.value, 10)
+    if (evt.target.className.includes('scale__control--smaller') && Scale.MIN < scale) {
+      scale -= Scale.STEP
+    } else if (evt.target.className.includes('scale__control--bigger') && Scale.MAX > scale) {
+      scale += Scale.STEP
+    }
+    scaleValue.value = scale + '%'
+    uploadPreview.style.transform = `scale(${scale/100})`
+  })
+})
--- build/js/effects.js
+++ build/js/effects.js
@@ -4,25 +4,107 @@ const rangeNoUiSlider = document.querySelector('.effect-level__slider')
 const effectLevelSlider = document.querySelector('.effect-level__value')
 const uploadPreview = document.querySelector('.img-upload__preview > img')
 const effectRadioGroup = document.getElementsByName('effect')
+const fieldEffectLevel = document.querySelector('.effect-level')
+
+const NoUiSliderOptions = {
+  none: { options: {
+    range: {
+      'min': 0,
+      'max': 1,
+    },
+    start: 1,
+    step: 0.01,
+  },
+  effect: '',
+  unit: '',
+  },
+  chrome: { options: {
+    range: {
+      'min': 0,
+      'max': 1,
+    },
+    start: 1,
+    step: 0.01,
+  },
+  effect: 'grayscale',
+  unit: '',
+  },
+  sepia: { options: {
+    range: {
+      'min': 0,
+      'max': 1,
+    },
+    start: 1,
+    step: 0.01,
+  },
+  effect: 'sepia',
+  unit: '',
+  },
+  marvin: { options: {
+    range: {
+      'min': 0,
+      'max': 100,
+    },
+    start: 100,
+    step: 0.1,
+  },
+  effect: 'invert',
+  unit: '%',
+  },
+  phobos: { options: {
+    range: {
+      'min': 0,
+      'max': 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+  effect: 'blur',
+  unit: 'px',
+  },
+  heat: { options: {
+    range: {
+      'min': 1,
+      'max': 3,
+    },
+    start: 3,
+    step: 0.1,
+  },
+  effect: 'brightness',
+  unit: '',
+  },
+}
 
 noUiSlider.create(rangeNoUiSlider, {
   start: 100,
   connect: 'lower',
-  step: 1,
+  step: 0.01,
   range: {
     'min': 0,
-    'max': 100,
+    'max': 1,
   },
 });
 
-rangeNoUiSlider.noUiSlider.on('update', (_, handle, unencoded) => {
-  effectLevelSlider.value = unencoded[handle];
-})
-
-console.log(effectRadioGroup)
-function setColor(event) {
-  console.log(event.target.value)
-  uploadPreview.className = `effects__preview--${event.target.value}`;
+function setFilter(event) {
+  const value = event.target.value
+  if (value == 'none') {
+    setDefaultOptions ()
+  } else {
+    uploadPreview.className = `effects__preview--${value}`;
+    fieldEffectLevel.classList.remove('hidden')
+    rangeNoUiSlider.noUiSlider.on('update', (_, handle, unencoded) => {
+      effectLevelSlider.value = unencoded[handle];
+      uploadPreview.style.filter = `${NoUiSliderOptions[value].effect}(${effectLevelSlider.value + NoUiSliderOptions[value].unit})`
+    })
+  }
+  rangeNoUiSlider.noUiSlider.updateOptions(NoUiSliderOptions[value].options)
 }
-for (let i = 0; i < effectRadioGroup.length; i++)
-  effectRadioGroup[i].addEventListener('change', setColor);
+
+function setDefaultOptions () {
+  fieldEffectLevel.classList.add('hidden')
+  uploadPreview.removeAttribute('class')
+  uploadPreview.removeAttribute('style')
+  rangeNoUiSlider.noUiSlider.off()
+}
+
+export {setDefaultOptions, effectRadioGroup, setFilter}
